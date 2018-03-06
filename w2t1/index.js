49015be2206cbb59aa82d87d9d976115444dcc96
Roman Strakhov <roman@strakhov.ru>
2018-03-06 06:03:21 +0300

    w2 programming made
    .at, .removeAt implemented

--- w2t1/index.js
+++ w2t1/index.js
@@ -30,7 +30,7 @@ Collection.prototype.append = function(item) {
 		console.log("Append: "+item+" total: "+this.__values.length);
 		// console.info(item);
 	}
-}
+};
 
 Collection.prototype.count = function() {
 	console.log("Counting:");
@@ -38,7 +38,36 @@ Collection.prototype.count = function() {
 	console.log("=> " + this.__values.length);
 
 	return this.__values.length;
-}
+};
+
+Collection.prototype.at = function(position) {
+
+	console.log("At("+position+"):");
+
+	if ((position <= 0) || (position>this.__values.length)) {
+		console.log("=> no element");
+		return null;
+	} else {
+		console.log("=> "+this.__values[position-1]);
+		return this.__values[position-1];
+	};
+
+};
+
+Collection.prototype.removeAt = function(position) {
+
+	console.log("RemoveAt("+position+"):");
+
+	if ((position <= 0) || (position>this.__values.length)) {
+		console.log("=> no element");
+		return false;
+	} else {
+		this.__values.splice(position-1,1);
+		console.log("=> "+this.__values[position-1]);
+		return true;
+	};
+
+};
 
 
 /**
